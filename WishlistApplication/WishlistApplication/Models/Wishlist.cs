cb9158324fe1cee11e4317baa3e94533a1a4891d
Orlando <lopezorlando23@gmail.com>
2020-12-09 10:49:35 -0700

    Wishlist Class added
    Added the Wishlist class to the Model folder

---
+++ WishlistApplication/WishlistApplication/Models/Wishlist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishlistApplication.Models
+{
+    public class Wishlist
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
